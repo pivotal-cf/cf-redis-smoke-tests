0e8c261bd2fdcc7e424cbf323e5b3a9dccead6e4
Henry Stanley <hstanley@pivotal.io>
2016-07-07 16:44:54 +0100

    improve error logging
    Signed-off-by: Jonathan Qiwen Tsang <jtsang@pivotal.io>

--- service/service_test.go
+++ service/service_test.go
@@ -36,12 +36,11 @@ var _ = Describe("Redis Service", func() {
 	assertAppIsRunning := func(appName string) {
 		pingUri := appUri(appName) + "/ping"
 		fmt.Println("Checking that the app is responding at url: ", pingUri)
-		Eventually(runner.Curl(pingUri, "-k"), shortTimeout, retryInterval).Should(Say("key not present"))
+		Eventually(runner.Curl(pingUri, "-k"), shortTimeout, retryInterval).Should(Say("key not present"), "Test app deployed but did not respond in time")
 		fmt.Println()
 	}
 
 	createTestOrgAndSpace := func() {
-		fmt.Println(testConfig)
 		Eventually(cf.Cf("auth", testConfig.AdminUser, testConfig.AdminPassword), shortTimeout).Should(Exit(0), "Failed to `cf auth` with target Cloud Foundry")
 		Eventually(cf.Cf("create-org", testConfig.OrgName), shortTimeout).Should(Exit(0), "Failed to create CF test org")
 		Eventually(cf.Cf("target", "-o", testConfig.OrgName)).Should(Exit(0))
@@ -61,7 +60,7 @@ var _ = Describe("Redis Service", func() {
 	})
 
 	AfterEach(func() {
-		Eventually(cf.Cf("delete", appName, "-f"), shortTimeout).Should(Exit(0))
+		Eventually(cf.Cf("delete", appName, "-f"), shortTimeout).Should(Exit(0), "Failed to `cf delete` test app")
 	})
 
 	AfterSuite(func() {
@@ -79,26 +78,44 @@ var _ = Describe("Redis Service", func() {
 
 			select {
 			case <-createServiceStdout.Detect("FAILED"):
-				Eventually(createServiceSession, shortTimeout).Should(Say("instance limit for this service has been reached"))
+				Eventually(createServiceSession, shortTimeout).Should(Say("instance limit for this service has been reached"),
+					"Failed to bind Redis service instance to test app",
+				)
 				Eventually(createServiceSession, shortTimeout).Should(Exit(1))
-				fmt.Println("No Plan Instances available for testing plan:", planName)
+				fmt.Println("No Plan Instances available for testing " + planName + " plan")
+
 			case <-createServiceStdout.Detect("OK"):
-				Eventually(createServiceSession, shortTimeout).Should(Exit(0))
-				Eventually(cf.Cf("bind-service", appName, serviceInstanceName), shortTimeout).Should(Exit(0))
-				Eventually(cf.Cf("start", appName), longTimeout).Should(Exit(0))
+				Eventually(createServiceSession, shortTimeout).Should(Exit(0),
+					"Failed to create Redis service instance",
+				)
+				Eventually(cf.Cf("bind-service", appName, serviceInstanceName), shortTimeout).Should(Exit(0),
+					"Failed to bind Redis service instance to test app",
+				)
+				Eventually(cf.Cf("start", appName), longTimeout).Should(Exit(0),
+					"Failed to start test app",
+				)
+
 				assertAppIsRunning(appName)
 
 				uri := appUri(appName) + "/mykey"
 				fmt.Println("Posting to url: ", uri)
-				Eventually(runner.Curl("-d", "data=myvalue", "-X", "PUT", uri, "-k"), shortTimeout, retryInterval).Should(Say("success"))
+				Eventually(runner.Curl("-d", "data=myvalue", "-X", "PUT", uri, "-k"), shortTimeout, retryInterval).Should(Say("success"),
+					"Failed to write to test "+planName+" instance",
+				)
 				fmt.Println()
 
 				fmt.Println("Getting from url: ", uri)
-				Eventually(runner.Curl(uri, "-k"), shortTimeout, retryInterval).Should(Say("myvalue"))
+				Eventually(runner.Curl(uri, "-k"), shortTimeout, retryInterval).Should(Say("myvalue"),
+					"Failed to read from test "+planName+" instance",
+				)
 				fmt.Println()
 
-				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), shortTimeout).Should(Exit(0))
-				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), shortTimeout).Should(Exit(0))
+				Eventually(cf.Cf("unbind-service", appName, serviceInstanceName), shortTimeout).Should(Exit(0),
+					"Failed to unbind "+planName+" instance from test app",
+				)
+				Eventually(cf.Cf("delete-service", "-f", serviceInstanceName), shortTimeout).Should(Exit(0),
+					"Failed to delete test "+planName+" instance",
+				)
 			}
 			createServiceStdout.CancelDetects()
 
