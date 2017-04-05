52d4f28acaa78ff1e8128b4d01d81e2e6e0d65d1
Maria Ntalla <mntalla@pivotal.io>
2017-04-05 16:12:19 +0100

    Wrap remaining cf api calls in retry logic
    [#142931995]
    
    Signed-off-by: Hannes Hoerl <hhorl@pivotal.io>

--- cf/cf.go
+++ cf/cf.go
@@ -11,7 +11,6 @@ import (
 	"time"
 
 	. "github.com/onsi/gomega"
-	"github.com/onsi/gomega/gbytes"
 	"github.com/onsi/gomega/gexec"
 	helpersCF "github.com/pivotal-cf-experimental/cf-test-helpers/cf"
 	"github.com/pivotal-cf/cf-redis-smoke-tests/retry"
@@ -28,15 +27,19 @@ type CF struct {
 
 //API is equivalent to `cf api {endpoint} [--skip-ssl-validation]`
 func (cf *CF) API(endpoint string, skipSSLValidation bool) func() {
+	apiCmd := []string{"api", endpoint}
+
+	if skipSSLValidation {
+		apiCmd = append(apiCmd, "--skip-ssl-validation")
+	}
+
+	cfApiFn := func() *gexec.Session {
+		return helpersCF.Cf(apiCmd...)
+	}
+
 	return func() {
-		apiCmd := []string{"api", endpoint}
-
-		if skipSSLValidation {
-			apiCmd = append(apiCmd, "--skip-ssl-validation")
-		}
-
-		Eventually(helpersCF.Cf(apiCmd...), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(cfApiFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to target Cloud Foundry"}`,
 		)
 	}
@@ -44,9 +47,13 @@ func (cf *CF) API(endpoint string, skipSSLValidation bool) func() {
 
 //Auth is equivalent to `cf auth {user} {password}`
 func (cf *CF) Auth(user, password string) func() {
+	authFn := func() *gexec.Session {
+		return helpersCF.Cf("auth", user, password)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("auth", user, password), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(authFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			"{\"FailReason\": \"Failed to `cf auth` with target Cloud Foundry\"}",
 		)
 	}
@@ -54,11 +61,15 @@ func (cf *CF) Auth(user, password string) func() {
 
 //CreateQuota is equivalent to `cf create-quota {name} [args...]`
 func (cf *CF) CreateQuota(name string, args ...string) func() {
+	cfArgs := []string{"create-quota", name}
+	cfArgs = append(cfArgs, args...)
+	createQuotaFn := func() *gexec.Session {
+		return helpersCF.Cf(cfArgs...)
+	}
+
 	return func() {
-		cfArgs := []string{"create-quota", name}
-		cfArgs = append(cfArgs, args...)
-		Eventually(helpersCF.Cf(cfArgs...), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(createQuotaFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			"{\"FailReason\": \"Failed to `cf create-quota` with target Cloud Foundry\"}",
 		)
 	}
@@ -66,9 +77,13 @@ func (cf *CF) CreateQuota(name string, args ...string) func() {
 
 //CreateOrg is equivalent to `cf create-org {org} -q {quota}`
 func (cf *CF) CreateOrg(org, quota string) func() {
+	createOrgFn := func() *gexec.Session {
+		return helpersCF.Cf("create-org", org, "-q", quota)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("create-org", org, "-q", quota), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(createOrgFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to create CF test org"}`,
 		)
 	}
@@ -77,13 +92,20 @@ func (cf *CF) CreateOrg(org, quota string) func() {
 //EnableServiceAccess is equivalent to `cf enable-service-access -o {org} {service-offering}`
 //In order to run enable-service-access idempotently we disable-service-access before.
 func (cf *CF) EnableServiceAccess(org, service string) func() {
+	disableServiceAccessFn := func() *gexec.Session {
+		return helpersCF.Cf("disable-service-access", "-o", org, service)
+	}
+	enableServiceAccessFn := func() *gexec.Session {
+		return helpersCF.Cf("enable-service-access", "-o", org, service)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("disable-service-access", "-o", org, service), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(disableServiceAccessFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to disable service access for CF test org"}`,
 		)
-		Eventually(helpersCF.Cf("enable-service-access", "-o", org, service), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(enableServiceAccessFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to enable service access for CF test org"}`,
 		)
 	}
@@ -91,9 +113,12 @@ func (cf *CF) EnableServiceAccess(org, service string) func() {
 
 //TargetOrg is equivalent to `cf target -o {org}`
 func (cf *CF) TargetOrg(org string) func() {
+	targetOrgFn := func() *gexec.Session {
+		return helpersCF.Cf("target", "-o", org)
+	}
 	return func() {
-		Eventually(helpersCF.Cf("target", "-o", org), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(targetOrgFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to target test org"}`,
 		)
 	}
@@ -101,9 +126,13 @@ func (cf *CF) TargetOrg(org string) func() {
 
 //TargetOrgAndSpace is equivalent to `cf target -o {org} -s {space}`
 func (cf *CF) TargetOrgAndSpace(org, space string) func() {
+	targetFn := func() *gexec.Session {
+		return helpersCF.Cf("target", "-o", org, "-s", space)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("target", "-o", org, "-s", space), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(targetFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to target test org"}`,
 		)
 	}
@@ -111,9 +140,13 @@ func (cf *CF) TargetOrgAndSpace(org, space string) func() {
 
 //CreateSpace is equivalent to `cf create-space {space}`
 func (cf *CF) CreateSpace(space string) func() {
+	createSpaceFn := func() *gexec.Session {
+		return helpersCF.Cf("create-space", space)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("create-space", space), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(createSpaceFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to create CF test space"}`,
 		)
 	}
@@ -156,9 +189,13 @@ func (cf *CF) CreateAndBindSecurityGroup(securityGroup, appName, org, space stri
 
 //DeleteSecurityGroup is equivalent to `cf delete-security-group {securityGroup} -f`
 func (cf *CF) DeleteSecurityGroup(securityGroup string) func() {
+	delSecGroupFn := func() *gexec.Session {
+		return helpersCF.Cf("delete-security-group", securityGroup, "-f")
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("delete-security-group", securityGroup, "-f"), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(delSecGroupFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to delete security group"}`,
 		)
 	}
@@ -166,23 +203,29 @@ func (cf *CF) DeleteSecurityGroup(securityGroup string) func() {
 
 //CreateUser is equivalent to `cf create-user {name} {password}`
 func (cf *CF) CreateUser(name, password string) func() {
+
+	createUserFn := func() *gexec.Session {
+		return helpersCF.Cf("create-user", name, password)
+	}
+
+	// if the user already exists, `cf create-user {name} {password}` is still OK
 	return func() {
-		createUserCmd := helpersCF.Cf("create-user", name, password)
-		Eventually(createUserCmd, cf.ShortTimeout).Should(gexec.Exit())
-		if createUserCmd.ExitCode() != 0 {
-			Expect(createUserCmd.Out).To(
-				gbytes.Say("scim_resource_already_exists"),
-				`{"FailReason": "Failed to create user"}`,
-			)
-		}
+		retry.Session(createUserFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
+			`{"FailReason": "Failed to create user"}`,
+		)
 	}
 }
 
 //DeleteUser is equivalent to `cf delete-user -f {name}`
 func (cf *CF) DeleteUser(name string) func() {
+	deleteUserFn := func() *gexec.Session {
+		return helpersCF.Cf("delete-user", "-f", name)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("delete-user", "-f", name), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(deleteUserFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to delete user"}`,
 		)
 	}
@@ -190,9 +233,13 @@ func (cf *CF) DeleteUser(name string) func() {
 
 //SetSpaceRole is equivalent to `cf set-space-role {name} {org} {space} {role}`
 func (cf *CF) SetSpaceRole(name, org, space, role string) func() {
+	setSpaceRoleFn := func() *gexec.Session {
+		return helpersCF.Cf("set-space-role", name, org, space, role)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("set-space-role", name, org, space, role), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(setSpaceRoleFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to set space role"}`,
 		)
 	}
@@ -202,9 +249,14 @@ func (cf *CF) SetSpaceRole(name, org, space, role string) func() {
 func (cf *CF) Push(appName string, args ...string) func() {
 	pushArgs := []string{"push", appName}
 	pushArgs = append(pushArgs, args...)
+
+	pushFn := func() *gexec.Session {
+		return helpersCF.Cf(pushArgs...)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf(pushArgs...), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(pushFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			"{\"FailReason\": \"Failed to `cf push` test app\"}",
 		)
 	}
@@ -212,9 +264,13 @@ func (cf *CF) Push(appName string, args ...string) func() {
 
 //Delete is equivalent to `cf delete {appName} -f`
 func (cf *CF) Delete(appName string) func() {
+	deleteAppFn := func() *gexec.Session {
+		return helpersCF.Cf("delete", appName, "-f", "-r")
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("delete", appName, "-f", "-r"), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(deleteAppFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			"{\"FailReason\": \"Failed to `cf delete` test app\"}",
 		)
 	}
@@ -222,26 +278,33 @@ func (cf *CF) Delete(appName string) func() {
 
 //CreateService is equivalent to `cf create-service {serviceName} {planName} {instanceName}`
 func (cf *CF) CreateService(serviceName, planName, instanceName string, skip *bool) func() {
-	return func() {
-		session := helpersCF.Cf("create-service", serviceName, planName, instanceName)
-		session.Wait(cf.ShortTimeout)
-		createServiceStdout := session.Out
+	createServiceFn := func() *gexec.Session {
+		return helpersCF.Cf("create-service", serviceName, planName, instanceName)
+	}
 
-		defer createServiceStdout.CancelDetects()
-		select {
-		case <-createServiceStdout.Detect("FAILED"):
-			Eventually(session, cf.ShortTimeout).Should(
-				gbytes.Say("instance limit for this service has been reached"),
-				`{"FailReason": "Failed to bind Redis service instance to test app"}`,
-			)
-			Eventually(session, cf.ShortTimeout).Should(gexec.Exit(1))
-			fmt.Printf("No Plan Instances available for testing %s plan\n", planName)
-			*skip = true
-		case <-createServiceStdout.Detect("OK"):
-			Eventually(session, cf.ShortTimeout).Should(
-				gexec.Exit(0),
-				`{"FailReason": "Failed to create Redis service instance"}`,
-			)
+	var SucceedsOrQuotaReached = func() retry.Condition {
+		return func(session *gexec.Session) bool {
+			actualSuccess := regexp.MustCompile("OK").Match(session.Out.Contents()) && session.ExitCode() == 0
+			failureBecauseQuotaReached :=
+				regexp.MustCompile("FAILED").Match(session.Out.Contents()) && (
+				// legacy release
+				regexp.MustCompile("instance limit for this service has been reached").Match(session.Out.Contents()) ||
+					// ODB
+					regexp.MustCompile("The quota for this service plan has been exceeded.").Match(session.Out.Contents())) &&
+					session.ExitCode() == 1
+			if failureBecauseQuotaReached {
+				fmt.Printf("No Plan Instances available for testing %s plan\n", planName)
+				*skip = true
+			}
+			return actualSuccess || failureBecauseQuotaReached
+		}
+	}
+
+	return func() {
+		retry.Session(createServiceFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(SucceedsOrQuotaReached(),
+			`{"FailReason": "Failed to create Redis service instance"}`,
+		)
+		if !(*skip) {
 			cf_helpers.AwaitServiceCreation(instanceName)
 		}
 	}
@@ -307,9 +370,13 @@ func (cf *CF) BindService(appName, instanceName string) func() {
 
 //UnbindService is equivalent to `cf unbind-service {appName} {instanceName}`
 func (cf *CF) UnbindService(appName, instanceName string) func() {
+	unbindFn := func() *gexec.Session {
+		return helpersCF.Cf("unbind-service", appName, instanceName)
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("unbind-service", appName, instanceName), cf.ShortTimeout).Should(
-			gexec.Exit(0),
+		retry.Session(unbindFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.MatchesStdOrErrorOutput(regexp.MustCompile(fmt.Sprintf("(OK|Service instance %s not found)", instanceName))),
 			fmt.Sprintf(`{"FailReason": "Failed to unbind %s instance from %s"}`, instanceName, appName),
 		)
 	}
@@ -337,9 +404,13 @@ func (cf *CF) SetEnv(appName, environmentVariable, instanceName string) func() {
 
 //Logout is equivalent to `cf logout`
 func (cf *CF) Logout() func() {
+	logoutFn := func() *gexec.Session {
+		return helpersCF.Cf("logout")
+	}
+
 	return func() {
-		Eventually(helpersCF.Cf("logout")).Should(
-			gexec.Exit(0),
+		retry.Session(logoutFn).WithSessionTimeout(cf.ShortTimeout).AndMaxRetries(cf.MaxRetries).AndBackoff(cf.RetryBackoff).Until(
+			retry.Succeeds,
 			`{"FailReason": "Failed to logout"}`,
 		)
 	}
