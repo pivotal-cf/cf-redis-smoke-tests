16fa40f6da27c6d5da7d62971878ee5a559be2ae
Ed Cook <ecook@pivotal.io>
2018-08-30 18:17:33 +0100

    Remove security group deletion for service tests
    [#159919674]

--- service/service_test.go
+++ service/service_test.go
@@ -182,10 +182,6 @@ var _ = Describe("Redis Service", func() {
 				"Delete the app",
 				testCF.Delete(appName),
 			),
-			reporter.NewStep(
-				fmt.Sprintf("Delete security group '%s'", securityGroupName),
-				testCF.DeleteSecurityGroup(securityGroupName),
-			),
 		}
 
 		smokeTestReporter.RegisterSpecSteps(specSteps)
